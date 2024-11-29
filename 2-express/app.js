82ad23d3b8d015f2ec150b5ed311fece7f8a859e
gabriel-belo <rm551669@fiap.com.br>
2024-11-29 13:01:45 -0300

    organizando o app.js e adicionando os middlewars a outros arquivos

--- 2-express/app.js
+++ 2-express/app.js
@@ -1,6 +1,7 @@
 const express= require('express')
 const app= express()
-const { people }= require('./data')
+const people  = require('./routes/people')
+const auth  = require('./routes/auth')
 
 //ativos staticos
 app.use(express.static('./methods-public'))
@@ -11,65 +12,10 @@ app.use(express.urlencoded({extended: false}))
 //analisar json
 app.use(express.json())
 
-app.get('/api/people', (req, res)=>{
-    res.status(200).json({success: true, data: people})
-})
+app.use('/api/people', people )
 
-app.post('/api/postman/people', (req, res) =>{
-    const{name}= req.body
-    if(!name){
-        return res
-        .status(400)
-        .json({success: false, msg: "please provide name value"})
-    }
-    res.status(201).json({success: true, data: [...people, name]})
-})
-app.post('/api/people', (req, res)=>{
-    const {name}= req.body
-    if(!name){
-        return res.status(400).json({success: false, msg: 'please provide name value'})
-    }
-    res.status(201).json({success: true, person: name}) 
-})
-app.post('/login', (req, res)=>{
-    const {name}= req.body
-    if(name){
-        return res.status(200).send(`Welcome ${name}`)
-    }
-    res.status(200).send('Please Provide Credentials') 
-})
+app.use('/login', auth)
 
-
-app.put('/api/people/:id', (req, res)=>{
-    const {id}= req.params
-    const {name}= req.body
-    
-    const person= people.find((person) => person.id === Number(id))
-
-    if(!person){
-        return res.status(404).json({success: false, msg: `no person with id ${id}`})
-    }
-    const newPeople= people.map((person)=>{
-        if(person.id === Number(id)){
-            person.name = name
-        }
-        return person
-    })
-    res.status(200).json({success: true, data: newPeople}) 
-})
-
-app.delete(('/api/people/:id'), (req, res)=>{
-    const person= people.find((person)=> person.id === Number(req.params.id)) //Neste caso esta sendo acessado diretamente o parametro do objeto 
-
-    if(!person){
-        return res
-        .status(404)
-        .json({success: false, msg: `no person qith id ${req.params.id}`})
-    }
-
-    const newPeople= people.filter((person)=> person.id !== Number(req.params.id))
-    return res.status(200).json({success: true, data: newPeople})
-})
 
 app.listen(5000, 
     console.log("Server is listening on port 5000...")
---
+++ 2-express/controllers/people.js
@@ -0,0 +1,3 @@
+const getPeople= (req, res)=>{
+    res.status(200).json({success: true, data: people})
+}
