4574b7382c023929a41b21efca712021d986e373
gabriel-belo <rm551669@fiap.com.br>
2024-11-28 21:08:34 -0300

    método put

--- 2-express/app.js
+++ 2-express/app.js
@@ -39,6 +39,25 @@ app.post('/login', (req, res)=>{
     res.status(200).send('Please Provide Credentials') 
 })
 
+
+app.put('/api/people/:id', (req, res)=>{
+    const {id}= req.params
+    const {name}= req.body
+    
+    const person= people.find((person) => person.id === Number(id))
+
+    if(!person){
+        return res.status(404).json({success: false, msg: `no person with id ${id}`})
+    }
+    const newPeople= people.map((person)=>{
+        if(person.id === Number(id)){
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({success: true, data: newPeople}) 
+})
+
 app.listen(5000, 
     console.log("Server is listening on port 5000...")
 ) 
